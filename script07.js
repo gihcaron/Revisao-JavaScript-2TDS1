5558ecf14faf2c14c1e36125c49f5b4b36ec6499
Giovanna Caron <giovannacaron.tds1.senai@gmail.com>
2024-08-08 11:44:49 -0300

    Feat - Creat Script07

--- script07.js
+++ script07.js
@@ -1,2 +1,17 @@
 
-let 
+let metaLivro = [10];
+
+let livrosLidos = ["Astrid Parker Never Fails", "A Hora da Estrela", "Senhora", "Revolução dos Bichos", "A Biblioteca da Meia Noite", "7 Maridos de Evelyn Hugo", "Diário de Um Banana", "Rainha Vermelha"]
+
+
+console.log("Confira só os seus livros lidos 🤯: ")
+for (let i = 0; i < livrosLidos.length; i++) {           console.log(` 📘 | ${livrosLidos[i]}`);
+   
+} 
+
+if (livrosLidos == metaLivro) {
+    console.log( "Parabéns! Você atingiu sua meta de leitura! 🤩🤗")
+
+} else {
+            console.log ( `Você está progredindo! Faltam ${metaLivro - livrosLidos.length} livros para atingir sua meta.`);
+        } 
